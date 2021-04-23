edf9d0f9124ac1b7193a4444db46570200e97831
congong <hebeicongcong@126.com>
2021-04-23 10:58:33 +0800

    up

---
+++ js/JSClickBubble.user.js
@@ -0,0 +1,61 @@
+// ==UserScript==
+// @name         鼠标点击冒泡
+// @namespace    https://djzhao.js.org
+// @version      0.3.2
+// @description  一个用JS写的鼠标左击特效
+// @description  一些Emoji 😀😃😄😁😆😅😂🤣☺😊😚😙😗😘😍😌😉🙃🙂😇😋😜😝😛🤑🤗🤓😎🤡🤠😖😣☹🙁😕😟😔😞😒😏😫😩😤😠😡😶😐😑😯😦😥😢😨😱😳😵😲😮😧🤤😭😪😴🙄🤔😬🤥🤐💩👺👹👿😈🤕🤒😷🤧🤢👻💀☠👽👾🤖🎃😺😸😹🙏👏🙌👐😾😿🙀😽😼😻
+// @author       一碗单炒饭
+// @include      /[a-zA-z]+://[^\s]*/
+// @run-at       document_start
+// ==/UserScript==
+onload = function() {
+    var click_cnt = 0;
+    var $html = document.getElementsByTagName("html")[0];
+    var $body = document.getElementsByTagName("body")[0];
+
+    var emoji ="❤😀😃😄😁😆😅😂🤣☺😊😚😙😗😘😍😌😉🙃🙂😇😋😜😝😛🤑🤗🤓😎🤡🤠😖😣☹🙁😕😟😔😞😒😏😫😩😤😠😡😶😐😑😯😦😥😢😨😱😳😵😲😮😧🤤😭😪😴🙄🤔😬🤥🤐💩👺👹👿😈🤕🤒😷🤧🤢👻💀☠👽👾🤖🎃😺😸😹🙏👏🙌👐😾😿🙀😽😼😻"
+
+    $html.onclick = function(e) {
+        var $elem = document.createElement("b");
+        // var single_emoji=emoji.substr(click_cnt*2,2)
+        // console.log(single_emoji)
+        ++click_cnt
+
+        $elem.innerText ="❤"
+        // $elem.innerText =single_emoji
+
+        // $elem.style.color = "red";
+        $elem.style.color = mycolor();
+        $elem.style.zIndex = 9999;
+        $elem.style.position = "absolute";
+        $elem.style.select = "none";
+        var x = e.pageX;
+        var y = e.pageY;
+        $elem.style.left = (x - 10) + "px";
+        $elem.style.top = (y - 20) + "px";
+        clearInterval(anim);
+
+
+        //字体随机大小
+        $elem.style.fontSize = Math.random() * 100 + 8 + "px";
+        var increase = 0;
+        var anim;
+        setTimeout(function() {
+        	anim = setInterval(function() {
+	            if (++increase == 150) {
+	                clearInterval(anim);
+			$body.removeChild($elem);
+	            }
+	            $elem.style.top = y - 20 - increase + "px";
+	            $elem.style.opacity = (150 - increase) / 120;
+	        }, 8);
+        }, 70);
+        $body.appendChild($elem);
+    };
+
+    //随机颜色
+    function mycolor() {
+        return "rgb(" + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + ")"
+    }
+
+};
